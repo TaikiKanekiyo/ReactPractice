d481604e3b8eb1d70461ae73174f426a5755c796
Taiki <taiki,kanekiyo@outlook.jp>
2023-05-26 23:28:16 +0900

    I added strile through functions

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ $("#addTodo").click(function () {
   $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>"); //inputtodoの要素をulタグ#todoListに追加(append)する
   $("input").val("");
 });
+
+$(document).on("change", "input[type=checkbox]", function () {
+  if ($(this).is(":checked")) {
+    $(this).parent().css("text-decoration", "line-through");
+  } else {
+    $(this).parent().css("text-decoration", "none");
+  }
+});
